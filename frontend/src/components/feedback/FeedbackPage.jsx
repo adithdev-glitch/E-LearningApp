b3f12141083b87aa327a68a24228be39d839c1e0
adithdev-glitch <adith.git46@gmail.com>
2025-11-20 16:08:07 +0530

    Fix casing of CSS import in FeedbackPage.jsx

--- frontend/src/components/feedback/FeedbackPage.jsx
+++ frontend/src/components/feedback/FeedbackPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Feedback.css';  
+import './feedback.css';  
 import { toast } from 'react-hot-toast';  
 import { server } from "../../main";
 
